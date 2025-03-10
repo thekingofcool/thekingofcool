a4e62715f798e9e5776a45c1e2f0644255f765d8
Fuck the Name <offweiboforawhile@proton.me>
2025-03-10 15:19:28 +0800

    Update fetch_strava_activities.py

--- fetch_strava_activities.py
+++ fetch_strava_activities.py
@@ -8,7 +8,6 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
 
-
 def refresh_access_token():
     url = "https://www.strava.com/oauth/token"
     payload = {
@@ -22,7 +21,6 @@ def refresh_access_token():
         tokens = response.json()
         return tokens['access_token'], tokens['refresh_token']
     else:
-        print(response.text)  # 打印错误响应
         raise Exception(f"Error refreshing access token: {response.status_code}")
 
 def get_strava_stats(access_token):
@@ -53,8 +51,6 @@ def get_strava_activities(access_token):
 
 def update_readme(stats, activities):
     readme_path = "README.md"
-    with open(readme_path, "r") as file:
-        readme_content = file.read()
 
     # 计算当前年度总距离
     current_year = time.localtime().tm_year
@@ -69,7 +65,6 @@ def update_readme(stats, activities):
     # 更新 README 内容
     new_content = f"""
     ## Strava Statistics
-
     - Name: {stats['firstname']} {stats['lastname']}
     - Total Distance (Current Year): {total_distance_current_year / 1000:.2f} km
     - Marathon PB: {marathon_pb // 3600}:{(marathon_pb % 3600) // 60}:{marathon_pb % 60} (HH:MM:SS) if marathon_pb else "N/A"
@@ -79,7 +74,7 @@ def update_readme(stats, activities):
     """
 
     with open(readme_path, "w") as file:
-        file.write(readme_content + new_content)
+        file.write(new_content)
 
 if __name__ == "__main__":
     access_token, refresh_token = refresh_access_token()
